7c13ea7a77a19125e7dbcf6e50c5f7567ed4729d
SekulDev <wojowniksek@gmail.com>
2025-09-27 14:57:04 +0200

    Initial Commit

---
+++ GithubContributionWidgetMacOS/GithubContributionWidgetMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubContributionWidgetMacOSApp.swift
+//  GithubContributionWidgetMacOS
+//
+//  Created by Sekul on 27.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct GithubContributionWidgetMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
