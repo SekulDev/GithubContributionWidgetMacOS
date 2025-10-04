3a113d1a85615ec55d305c86b38055b825cb4174
SekulDev <wojowniksek@gmail.com>
2025-10-04 14:55:58 +0200

    feat: closing whole app after close last window

--- GithubContributionWidgetMacOS/GithubContributionWidgetMacOSApp.swift
+++ GithubContributionWidgetMacOS/GithubContributionWidgetMacOSApp.swift
@@ -7,10 +7,17 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true  // Closing app after close last window
+    }
+}
+
 @main
 struct GithubContributionWidgetMacOSApp: App {
 
     @Environment(\.openURL) var openURL
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
         WindowGroup {
